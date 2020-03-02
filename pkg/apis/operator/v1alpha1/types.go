9406e92e0deed1cac6cee79050f70db16347bac7
Fabio Bertinatto <fbertina@redhat.com>
2020-03-02 17:27:41 +0100

    Update API

--- pkg/apis/operator/v1alpha1/types.go
+++ pkg/apis/operator/v1alpha1/types.go
@@ -19,33 +19,12 @@ type EBSCSIDriver struct {
 
 // EBSCSIDriverSpec is the spec for a EBSCSIDriver resource
 type EBSCSIDriverSpec struct {
-	// managementState indicates whether and how the operator should manage the component
-	// +optional
-	ManagementState operatorv1.ManagementState `json:"managementState,omitempty"`
-	// logLevel is an intent based logging for an overall component.  It does not give fine grained control, but it is a
-	// simple way to manage coarse grained logging choices that operators have to interpret for their operands.
-	// +optional
-	LogLevel operatorv1.LogLevel `json:"logLevel,omitempty"`
+	operatorv1.OperatorSpec `json:",inline"`
 }
 
 // EBSCSIDriverStatus is the status for a EBSCSIDriver resource
 type EBSCSIDriverStatus struct {
-	// ObservedGeneration is the last generation of this object that
-	// the operator has acted on.
-	ObservedGeneration *int64 `json:"observedGeneration,omitempty"`
-
-	// state indicates what the operator has observed to be its current operational status.
-	// State operatorv1.ManagementState `json:"managementState,omitempty"`
-
-	// Conditions is a list of conditions and their status.
-	Conditions []operatorv1.OperatorCondition `json:"conditions,omitempty"`
-
-	// readyReplicas indicates how many replicas are ready and at the desired state
-	ReadyReplicas int32 `json:"readyReplicas"`
-
-	// generations are used to determine when an item needs to be reconciled or has changed in a way that needs a reaction.
-	// +optional
-	Generations []operatorv1.GenerationStatus `json:"generations,omitempty"`
+	operatorv1.OperatorStatus `json:",inline"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
