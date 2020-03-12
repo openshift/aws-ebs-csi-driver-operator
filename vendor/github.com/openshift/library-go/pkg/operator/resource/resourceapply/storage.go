187962f55b1c1392a962adc4f183966a8c6207f7
Fabio Bertinatto <fbertina@redhat.com>
2020-03-12 13:55:51 +0100

    WIP: Add functions to read and apply CSIDriver objects
    This is temporary and should be moved to library-go eventually.

--- vendor/github.com/openshift/library-go/pkg/operator/resource/resourceapply/storage.go
+++ vendor/github.com/openshift/library-go/pkg/operator/resource/resourceapply/storage.go
@@ -4,10 +4,12 @@ import (
 	"k8s.io/klog"
 
 	storagev1 "k8s.io/api/storage/v1"
+	storagev1beta1 "k8s.io/api/storage/v1beta1"
 	"k8s.io/apimachinery/pkg/api/equality"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	storageclientv1 "k8s.io/client-go/kubernetes/typed/storage/v1"
+	storageclientv1beta1 "k8s.io/client-go/kubernetes/typed/storage/v1beta1"
 
 	"github.com/openshift/library-go/pkg/operator/events"
 	"github.com/openshift/library-go/pkg/operator/resource/resourcemerge"
@@ -45,6 +47,36 @@ func ApplyStorageClass(client storageclientv1.StorageClassesGetter, recorder eve
 
 	// TODO if provisioner, parameters, reclaimpolicy, or volumebindingmode are different, update will fail so delete and recreate
 	actual, err := client.StorageClasses().Update(existingCopy)
+	reportUpdateEvent(recorder, required, err)
+	return actual, true, err
+}
+
+// ApplyCSIDriverV1Beta1 merges objectmeta, does not worry about anything else
+func ApplyCSIDriverV1Beta1(client storageclientv1beta1.CSIDriversGetter, recorder events.Recorder, required *storagev1beta1.CSIDriver) (*storagev1beta1.CSIDriver, bool,
+	error) {
+	existing, err := client.CSIDrivers().Get(required.Name, metav1.GetOptions{})
+	if apierrors.IsNotFound(err) {
+		actual, err := client.CSIDrivers().Create(required)
+		reportCreateEvent(recorder, required, err)
+		return actual, true, err
+	}
+	if err != nil {
+		return nil, false, err
+	}
+
+	modified := resourcemerge.BoolPtr(false)
+	existingCopy := existing.DeepCopy()
+
+	resourcemerge.EnsureObjectMeta(modified, &existingCopy.ObjectMeta, required.ObjectMeta)
+	if !*modified {
+		return existingCopy, false, nil
+	}
+
+	if klog.V(4) {
+		klog.Infof("CSIDriver %q changes: %v", required.Name, JSONPatchNoError(existing, existingCopy))
+	}
+
+	actual, err := client.CSIDrivers().Update(existingCopy)
 	reportUpdateEvent(recorder, required, err)
 	return actual, true, err
 }
